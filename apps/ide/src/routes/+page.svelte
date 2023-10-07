931a9ebccb618d66824edb9bc2d7e5e2d6100f29
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2023-10-07 19:38:35 +0200

    fix(rust-analyzer): add cargo.toml on root

--- apps/ide/src/routes/+page.svelte
+++ apps/ide/src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api"
+  let name = "";
+  let result = "";
+
+  const handleSubmit = async () => {
+    result = await invoke("greet", { name });
+  }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<form on:submit={handleSubmit}>
+  <input bind:value={name} name="name" />
+  <button type="submit">Submit</button>
+</form>
+<span>{result}</span>
